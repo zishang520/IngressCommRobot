a50421a21df07caa11fc8259e2ff22dac0b1cde4
zishang520 <zishang520@gmail.com>
2016-09-19 13:03:01 +0800

    Some boring updates

--- ingress.php
+++ ingress.php
@@ -5,32 +5,38 @@
  */
 class ingress
 {
-    //请求头信息
+    // Request Header
     private $header = array();
-    //是都设置ssl
-    public $ssl = false;
-    //加载消息最近时间(分钟)
+    // Whether to open SSL
+    public $ssl = true;
+    // Load message last time (min)
     private $mintime;
-    //数据库连接
+    // Database connection
     private $sqllite;
-    // 配置文件
+    // configuration file
     private $conf;
-    // cookie 文件名
+    // Cookie file name
     private $cookie_file = 'cookie.ini';
-    //构造函数
+
+    /**
+     * [__construct description]
+     * @Author    ZiShang520@gmail.com
+     * @DateTime  2016-09-19T12:14:55+0800
+     * @copyright (c)                      ZiShang520 All           Rights Reserved
+     * @param     integer                  $mintime   [Load message last time (min)]
+     */
     public function __construct($mintime = 10)
     {
         $this->sqllite = new SQLite3('agent.db', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
         file_exists('conf.json') || exit('conf.json Not Found');
         $conf = file_get_contents('conf.json');
-        $conf = json_decode(preg_replace('/((\r|\n|^\s*)+(\/\/[^\n]*|(\/\*([^\*^\/]*|[\*^\/\*]*|[^\**\/]*)*\*\/)*)|\r|\n|^\s+|\s+$)*/im', '', $conf), true);
+        $conf = json_decode(preg_replace('/((\r|\n|^\s*)+(\/\/[^\n]*|(\/\*([^\*^\/]*|[\*^\/\*]*|[^\**\/]*)*\*\/)*)|\r|\n|^\s+|\s+$)*/sim', '', $conf), true);
         $this->conf = $conf ? $conf : array();
         $this->check_conf();
         $this->mintime = $mintime;
-        $this->ssl = true;
         $this->header = array(
             'Cache-Control' => 'Cache-Control: max-age=0',
-            'User-Agent' => 'User-Agent: '.$this->conf['UA'],
+            'User-Agent' => 'User-Agent: ' . $this->conf['UA'],
             'Upgrade-Insecure-Requests' => '1',
             'Accept:' => 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Language' => 'Accept-Language: zh-CN,zh;q=0.8',
@@ -39,66 +45,83 @@ class ingress
             'X-CSRFToken' => 'X-CSRFToken: ' . $this->conf['x-csrftoken'],
         );
     }
-    //请求
+
+    /**
+     * [curl description]
+     * @Author    ZiShang520@gmail.com
+     * @DateTime  2016-09-19T12:15:19+0800
+     * @copyright (c)                      ZiShang520 All           Rights Reserved
+     * @param     [type]                   $url       [request url]
+     * @param     [type]                   $post      [post data]
+     * @param     array                    $header    [header]
+     * @param     boolean                  $cookie    [auto save cookie]
+     * @return    [type]                              [curl exec data]
+     */
     protected function curl($url, $post = null, $header = array(), $cookie = true)
     {
         $header = array_merge_recursive($this->header, $header);
-        $data = array(); //初始化空数组
-        $ch = curl_init(); //初始化curl
+        $data = array();
+        $ch = curl_init();
         curl_setopt_array($ch,
             array(
-                CURLOPT_URL => $url, //需要请求的地址
-                // CURLOPT_PROXY => 'http://127.0.0.1:8080', // 不支持https
-                CURLOPT_HTTPHEADER => $header, //设置header头
-                CURLOPT_AUTOREFERER => true, // 自动设置跳转地址
-                CURLOPT_FOLLOWLOCATION => true, //开启重定向
-                CURLOPT_COOKIE => $this->conf['cookie'],//设置cookie
-                CURLOPT_TIMEOUT => 30, //设置超时时间
-                CURLOPT_RETURNTRANSFER => true, //设定是否显示头信息
-                CURLOPT_HEADER => false, //设定是否输出页面内容
-                CURLOPT_NOBODY => false, //是否设置为不显示html的body
+                CURLOPT_URL => $url, //Request Url
+                // CURLOPT_PROXY => 'http://127.0.0.1:8080', // HTTPS could not run
+                CURLOPT_HTTPHEADER => $header, //Set Request Header
+                CURLOPT_AUTOREFERER => true, // Open Auto Referer
+                CURLOPT_FOLLOWLOCATION => true, //Open Auto Location
+                CURLOPT_COOKIE => $this->conf['cookie'], //Set Cookie
+                CURLOPT_TIMEOUT => 30, //Set Timeout
+                CURLOPT_RETURNTRANSFER => true, //Set Not Show Response Headers
+                CURLOPT_HEADER => false, //Set Not Output Header
+                CURLOPT_NOBODY => false, //Set Not Show Body
             )
         );
         if (!empty($post)) {
             $post = is_array($post) ? http_build_query($post) : $post;
             curl_setopt_array($ch,
                 array(
-                    CURLOPT_POST => true, //post提交方式
+                    CURLOPT_POST => true, //Post request
                     CURLOPT_POSTFIELDS => $post,
                 )
             );
         }
         if ($this->ssl) {
+            // Check whether the domain name is set in the certificate, and whether the match is provided with the host name.
             curl_setopt_array($ch,
                 array(
-                    CURLOPT_SSL_VERIFYPEER => true, // 只信任CA颁布的证书
-                    CURLOPT_CAINFO => __DIR__ . '/cacert.pem', // CA根证书（用来验证的网站证书是否是CA颁布）
+                    CURLOPT_SSL_VERIFYPEER => true, // Only trust the certificate issued by CA
+                    CURLOPT_CAINFO => __DIR__ . '/cacert.pem', // CA root certificate (used to verify whether the site certificate is issued by the CA)
                     CURLOPT_SSL_VERIFYHOST => 2,
                 )
-            ); // 检查证书中是否设置域名，并且是否与提供的主机名匹配
+            );
         }
         if ($cookie) {
-            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file); //存储cookie信息
-            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file); // use cookie
+            curl_setopt_array($ch,
+                array(
+                    CURLOPT_COOKIEJAR => $this->cookie_file, //Storing cookie information
+                    CURLOPT_COOKIEFILE => $this->cookie_file, // use cookie
+                )
+            );
         }
         $data['info'] = curl_exec($ch);
         $data['status'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
         return $data;
     }
-    //获取消息
+    //Get message
     public function get_msg()
     {
         $url = 'https://www.ingress.com/r/getPlexts';
         $header['content-type'] = 'content-type:application/json; charset=UTF-8';
         $data = '{"minLatE6":' . $this->conf['minLatE6'] . ',"minLngE6":' . $this->conf['minLngE6'] . ',"maxLatE6":' . $this->conf['maxLatE6'] . ',"maxLngE6":' . $this->conf['maxLngE6'] . ',"minTimestampMs":' . intval(microtime(true) * 1000 - 60000 * $this->mintime) . ',"maxTimestampMs":-1,"tab":"faction","ascendingTimestampOrder":true,"v":"' . $this->conf['v'] . '"}';
+        var_dump($data);
         $info = $this->curl($url, $data, $header);
         if ($info['status'] != 200) {
             return false;
         }
         return $info['info'];
     }
-    //发送消息
+    //send message
     public function send_msg($msg)
     {
         $url = 'https://www.ingress.com/r/sendPlext';
@@ -115,12 +138,12 @@ class ingress
             return $arr['result'];
         }
     }
-    // 自动兑换
+    // Passcode automatic exchange
     public function auto_passcode($code)
     {
         $url = 'https://www.ingress.com/r/redeemReward';
         $header['content-type'] = 'content-type:application/json; charset=UTF-8';
-        $data = '{"passcode":"'.$code.'","v":"'.$this->conf['v'].'"}';
+        $data = '{"passcode":"' . $code . '","v":"' . $this->conf['v'] . '"}';
         $info = $this->curl($url, $data, $header);
         if ($info['status'] != 200) {
             return false;
@@ -134,8 +157,7 @@ class ingress
         if (!$msg) {
             return 'get message error';
         }
-        $arr = json_decode($msg, true);
-        if (!$arr) {
+        if (!$arr = json_decode($msg, true)) {
             return 'message decode error';
         }
         if (empty($arr['result']) || !is_array($arr['result'])) {
@@ -144,6 +166,7 @@ class ingress
         $agents = '';
         $values = array();
         foreach ($arr['result'] as $value) {
+            var_dump($value[2]['plext']['text']);
             if ($agent = $this->check_new_agent($value[2]['plext']['text'])) {
                 $values[] = '("' . $agent . '",' . time() . ')';
                 $agents .= '@' . $agent . ' ';
@@ -199,10 +222,15 @@ class ingress
         if (empty($this->conf['rand_msg'])) {
             $data = array(
                 ' 欢迎新人，快来加入川渝蓝军群(群号126821831)，发现精彩内容。',
-                ' 欢迎选择加入抵抗军·川渝蓝军群(群号126821831)，一起为建设社会主义社会、实现人类的全面自由发展而奋斗吧',
-                ' 您已进入秋名山路段，此处常有老司机出没，加入川渝蓝军群(群号126821831)，寻找这里的老司机吧',
+                ' Welcome newcomers, Come join the Sichuan-Chengdu Blue Army group (group number 126821831), found exciting content.',
+                ' 欢迎选择加入抵抗军·川渝蓝军群(群号126821831)，一起为建设社会主义社会、实现人类的全面自由发展而奋斗吧。',
+                ' Welcome to join the Resistance Army · Sichuan-Chengdu Blue Army group (group number 126821831), together for the building of a socialist society, to achieve the full freedom of human development and struggle.',
+                ' 您已进入秋名山路段，此处常有老司机出没，加入川渝蓝军群(群号126821831)，寻找这里的老司机吧。',
+                ' You have entered the Autumn Hill section, where the old drivers often come and go, join the Sichuan-Chengdu Blue Army group (group number 126821831), looking for the old driver here.',
                 ' 欢迎加入熊猫抵抗军(群号126821831)，感谢你在与shapers的斗争中选择了人性与救赎，选择与死磕并肩同行。新人你好，我是死磕。',
-                ' ingrees亚洲 中国分区 川渝地区组织需要你！快来加入川渝蓝军群(群号126821831)',
+                ' Welcome to join the Panda Resistance Army (group number 126821831), thank you in the struggle with the shapers of choice of human nature and redemption, choose to walk side by side with the Sike. Hello new, I was Sike.',
+                ' ingrees亚洲 中国分区 川渝地区组织需要你！快来加入川渝蓝军群(群号126821831)。',
+                ' Ingrees Asia Chinese partition in Sichuan and Chengdu area organization needs you! Come and join the group of Sichuan and Chengdu (No. 126821831).',
             );
         } else {
             $data = $this->conf['rand_msg'];
